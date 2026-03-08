2d2ea734ef49a597e182e79ea829d58ab13bc3e7
Miskler <69055489+Miskler@users.noreply.github.com>
2026-03-08 15:56:51 +0300

    Update git_ops.py

--- bot/git_ops.py
+++ bot/git_ops.py
@@ -7,16 +7,22 @@ def run(cmd):
 
 
 def create_branch():
-
     branch = f"snapshot-bot-{int(time.time())}"
-
     run(["git", "checkout", "-b", branch])
-
     return branch
 
 
 def commit_changes():
+    run(["git", "config", "user.name", "github-actions[bot]"])
+    run(["git", "config", "user.email", "41898282+github-actions[bot]@users.noreply.github.com"])
 
     run(["git", "add", "tests/__snapshots__"])
+
+    r = subprocess.run(["git", "diff", "--cached", "--quiet"])
+    if r.returncode == 0:
+        return False
+
     run(["git", "commit", "-m", "update snapshots"])
     run(["git", "push", "origin", "HEAD"])
+
+    return True
